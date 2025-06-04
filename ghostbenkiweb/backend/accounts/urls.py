89f3786e3aa59119a0b5bec4c5ea4412deeb4760
Bantu-art <brianbaraza40@gmail.com>
2025-06-05 00:21:58 +0300

    feat: add logout URL routing to accounts app

--- ghostbenkiweb/backend/accounts/urls.py
+++ ghostbenkiweb/backend/accounts/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import LoginView, RegistrationView
+from .views import LoginView, RegistrationView, LogoutView
 
 app_name = 'accounts'
 
 urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),
     path('register/', RegistrationView.as_view(), name='register'),
+    path('logout/', LogoutView.as_view(), name='logout'),
 ]
